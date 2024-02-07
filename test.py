b1aeedf99a5fc3ec7e2324bcda04099225c531e2
CATHRINE7 <143362763+CATHRINE7@users.noreply.github.com>
2024-02-07 09:42:44 +0200

    Update test.py
    added libraries

--- test.py
+++ test.py
@@ -36,6 +36,10 @@ import numpy as np
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
 from recommenders.content_based import content_model
+from sklearn.feature_extraction.text import TfidfVectorizer #for vectorizing text data'
+from sklearn.preprocessing import StandardScaler #for numeri values
+from sklearn.decomposition import TruncatedSVD # for collaborative model
+
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
