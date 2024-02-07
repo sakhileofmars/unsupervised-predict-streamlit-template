12557a412d8f42c82e5be90c9518a82291a18516
CATHRINE7 <143362763+CATHRINE7@users.noreply.github.com>
2024-02-07 10:55:51 +0200

    Update test.py

--- test.py
+++ test.py
@@ -97,10 +97,11 @@ def main():
 
         if sys == 'Collaborative Based Filtering':
             if st.button("Recommend"):
+		    movies = pd.read_csv('resources/data/movies.csv')
                 try:
                     with st.spinner('Crunching the numbers...'):
 			    model = 'SVD'
-			    usable_data = title_list
+			    data['predict_score] = 
                         top_recommendations = collab_model(movie_list=fav_movies,
                                                            top_n=10)
                     st.title("We think you'll like:")
